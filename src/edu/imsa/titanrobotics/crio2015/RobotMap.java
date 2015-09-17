6196acee5f3a45d83ab01320491294077e675729
Nick Nusgart <nicknusgart@gmail.com>
2015-09-17 11:52:10 -0500

    Imported the code for our controllers and added motor ports to RobotMap.
    Both the controller and motor port numbers could be incorrect, we will need to check them.

--- src/edu/imsa/titanrobotics/crio2015/RobotMap.java
+++ src/edu/imsa/titanrobotics/crio2015/RobotMap.java
@@ -6,13 +6,11 @@ package edu.imsa.titanrobotics.crio2015;
  * floating around.
  */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+	/* 
+	 * Tank Drive Motor Ports.
+	 */
+     public static int frontLeftMotor = 0;
+     public static int backLeftMotor = 1;
+     public static int frontRightMotor = 2;
+     public static int backRightMotor = 2;
 }
---
+++ src/edu/imsa/titanrobotics/crio2015/controllers/Attack3.java
@@ -0,0 +1,60 @@
+package edu.imsa.titanrobotics.crio2015.controllers;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ *
+ * @author TitanRobotics
+ * @editor Nick Nusgart because this class did not follow any reasonable code
+ *         standards. (Jan 22, 2015)
+ */
+public class Attack3 extends Joystick {
+	/**
+	 * Construct an instance of a Logitech Attack3 joystick (The kind in the
+	 * KoP). Initialize Button6 and Button 7 for controlling the pickup relays
+	 * 
+	 * @param port
+	 *            The port on the driver station that the joystick is plugged
+	 *            into.
+	 * @return
+	 */
+	public Attack3(int port) {
+		super(port);
+	}
+
+	public double getXAxis() {
+		return getX();
+		// return getAxis(Joystick.AxisType.kX);
+	}
+
+	public double getYAxis() {
+		return getY();
+		// return getAxis(Joystick.AxisType.kY);
+	}
+
+	/**
+	 * Return the value of the Twist axis for this joystick This value is always
+	 * 0 since there is no twist axis.
+	 * 
+	 * @return 0 since there is no twist axis
+	 */
+	public double getTwist() {
+		return 0;
+	}
+
+	/**
+	 * Get a JoystickButton for the Command Subsystem OI Class
+	 *
+	 * @param button
+	 *            The button as an integer
+	 * @return JoystickButton
+	 */
+	public JoystickButton GetButton(int button) {
+		return (new JoystickButton(this, button));
+	}
+
+	public boolean getButton(int button) {
+		return (new JoystickButton(this, button)).get();
+	}
+}
